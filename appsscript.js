8cfb009de340ae8696c683dcc6d117bbbb8090b1
bereasonable13 <mzdano@gmail.com>
2025-09-08 08:17:05 -0500

    Update appsscript.js

---
+++ appsscript.js
@@ -0,0 +1,15 @@
+{
+  "timeZone": "America/Chicago",
+  "exceptionLogging": "STACKDRIVER",
+  "runtimeVersion": "V8",
+  "webapp": {
+    "executeAs": "USER_ACCESSING",
+    "access": "ANYONE"
+  },
+  "oauthScopes": [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/forms",
+    "https://www.googleapis.com/auth/script.external_request",
+    "https://www.googleapis.com/auth/drive.file"
+  ]
+}
